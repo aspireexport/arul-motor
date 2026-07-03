64de4f905b76afb75f907c7a9ce267175a88e9a3
aspireexport <aspireexport@gmail.com>
2026-07-03 21:18:20 +0530

    Modify PWA config with new base and start_url
    Updated base and start_url for PWA configuration.

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,24 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/arul-motor/",
+
   plugins: [
     react(),
 
     VitePWA({
       registerType: "autoUpdate",
 
-      includeAssets: [
-        "favicon.ico",
-        "apple-touch-icon.png",
-        "masked-icon.svg"
-      ],
-
-     manifest: {
-  name: "Arul Motor Billing",
-  short_name: "Arul Motor",
-  description: "Offline Billing System",
-  start_url: "/",
-  display: "standalone",
-  background_color: "#ffffff",
-  theme_color: "#123d8d"
-}    })
+      manifest: {
+        name: "Arul Motor Billing",
+        short_name: "Arul Motor",
+        description: "Offline Billing System",
+        start_url: "/arul-motor/",
+        scope: "/arul-motor/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#123d8d"
+      }
+    })
   ]
 });
