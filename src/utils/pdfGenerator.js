53ccce1f634c27124e123b4c52399bdc331eada8
aspireexport <aspireexport@gmail.com>
2026-07-06 19:02:59 +0530

    Switched to html2pdf

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -1,50 +1,33 @@
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
+import html2pdf from "html2pdf.js";
 
-const generatePDF = async () => {
-    const invoice = document.getElementById("invoice");
+const generatePDF = () => {
+  const element = document.getElementById("invoice");
 
-    if (!invoice) return;
+  if (!element) return;
 
-    const canvas = await html2canvas(invoice, {
-    scale: 3,
-    useCORS: true,
-    backgroundColor: "#ffffff",
+  const options = {
+    margin: 10,
+    filename: "Invoice.pdf",
+    image: {
+      type: "jpeg",
+      quality: 1,
+    },
+    html2canvas: {
+      scale: 3,
+      useCORS: true,
+      scrollY: 0,
+    },
+    jsPDF: {
+      unit: "mm",
+      format: "a4",
+      orientation: "portrait",
+    },
+    pagebreak: {
+      mode: ["avoid-all", "css", "legacy"],
+    },
+  };
 
-    windowWidth: invoice.scrollWidth,
-    windowHeight: invoice.scrollHeight,
-
-    width: invoice.scrollWidth,
-    height: invoice.scrollHeight,
-
-    scrollX: 0,
-    scrollY: 0,
-});
-
-    const imgData = canvas.toDataURL("image/png");
-
-    const pdf = new jsPDF("p", "mm", "a4");
-
-    const pageWidth = 210;
-    const pageHeight = 297;
-
-    const imgWidth = pageWidth;
-    const imgHeight = (canvas.height * imgWidth) / canvas.width;
-
-    let heightLeft = imgHeight;
-    let position = 0;
-
-    pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-    heightLeft -= pageHeight;
-
-    while (heightLeft > 0) {
-        position = heightLeft - imgHeight;
-        pdf.addPage();
-        pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-        heightLeft -= pageHeight;
-    }
-
-    pdf.save("Invoice.pdf");
+  html2pdf().set(options).from(element).save();
 };
 
 export default generatePDF;
