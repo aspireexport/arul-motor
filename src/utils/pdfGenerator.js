2cf72ef3bf55efd56073fa261b1d78ae76be5fda
aspireexport <aspireexport@gmail.com>
2026-07-06 18:51:01 +0530

    Fixed pdf generation

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -7,28 +7,34 @@ const generatePDF = async () => {
     if (!invoice) return;
 
     const canvas = await html2canvas(invoice, {
-        scale: 2,
+        scale: 3,
         useCORS: true,
         backgroundColor: "#ffffff",
+        scrollY: -window.scrollY,
     });
 
     const imgData = canvas.toDataURL("image/png");
 
     const pdf = new jsPDF("p", "mm", "a4");
 
-    const pageWidth = pdf.internal.pageSize.getWidth();
+    const pageWidth = 210;
+    const pageHeight = 297;
 
-    const pageHeight =
-        (canvas.height * pageWidth) / canvas.width;
+    const imgWidth = pageWidth;
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
 
-    pdf.addImage(
-        imgData,
-        "PNG",
-        0,
-        0,
-        pageWidth,
-        pageHeight
-    );
+    let heightLeft = imgHeight;
+    let position = 0;
+
+    pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+    heightLeft -= pageHeight;
+
+    while (heightLeft > 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+    }
 
     pdf.save("Invoice.pdf");
 };
